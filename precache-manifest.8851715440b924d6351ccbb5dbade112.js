2ea95eefb56aca3efff940770218de0f763fd64d
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-15 02:45:33 +0530

    Updates

---
+++ precache-manifest.8851715440b924d6351ccbb5dbade112.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3685f367fd866c66588463483848f9",
+    "url": "/react-pactice-website-1/index.html"
+  },
+  {
+    "revision": "ed2f5eb4e458c7500907",
+    "url": "/react-pactice-website-1/static/css/main.539a9694.chunk.css"
+  },
+  {
+    "revision": "d0aeef4a15d92596bcf4",
+    "url": "/react-pactice-website-1/static/js/2.06397c7f.chunk.js"
+  },
+  {
+    "revision": "8f1544b07245491ea591f336c2dcc0b8",
+    "url": "/react-pactice-website-1/static/js/2.06397c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2f5eb4e458c7500907",
+    "url": "/react-pactice-website-1/static/js/main.751ad6c6.chunk.js"
+  },
+  {
+    "revision": "dc023ff28c4228e1f204",
+    "url": "/react-pactice-website-1/static/js/runtime-main.0498d9ff.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/react-pactice-website-1/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/react-pactice-website-1/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/react-pactice-website-1/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/react-pactice-website-1/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/react-pactice-website-1/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/react-pactice-website-1/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
